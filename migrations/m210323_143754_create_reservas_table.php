82b138a6586ff0e3d14df72b0b0609a2498d7b89
Juan ANtonio Villoria del Moral <juvi.13.13@gmail.com>
2021-03-23 15:56:59 +0100

    Tabla reservas modificada

--- migrations/m210323_143754_create_reservas_table.php
+++ migrations/m210323_143754_create_reservas_table.php
@@ -16,7 +16,7 @@ class m210323_143754_create_reservas_table extends Migration
             'id' => $this->bigPrimaryKey(),
             'usuario_id' => $this->bigInteger()->notNull(),
             'vuelo_id' => $this->bigInteger()->notNull(),
-            'asiento' => $this->integer()->notNull()->check('asiento >= 0'),
+            'asiento' => $this->integer()->notNull()->check('asiento > 0'),
             'UNIQUE (vuelo_id, asiento)',
             'UNIQUE (usuario_id, vuelo_id)',
         ]);
